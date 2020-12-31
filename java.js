8946357ca7ff6f3a9d0e49cf5da51f21a27a65bf
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2020-12-31 16:07:51 +0400

    Add files via upload

--- java.js
+++ java.js
@@ -13,11 +13,29 @@ Webcam.set({
  
  function snap(){
      Webcam.snap(function(data_url){
-         document.getElementById("photo").innerHTML = "<img src='"+ data_url + "' width='325' height='275' style='margin-top: 10px;'>";
+         document.getElementById("photo").innerHTML = "<img src='"+ data_url + "' width='325' height='275' style='margin-top: 10px;' id='img1'>";
      });
  }
  console.log("ml5 version: " + ml5.version);
-brain_find = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/epyhIeZyZ/model.json", check);
+brain_find = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/j4BiXmw_s/model.json", check);
 function check(){
     console.log("modelLoaded");
 }
+function identify(){
+    imgGiven = document.getElementById("img1");
+    brain_find.classify(imgGiven, result);
+}
+function result(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+       document.getElementById("object").innerHTML = results[0].label;
+       document.getElementById("accuracy").innerHTML = (results[0].confidence.toFixed(3)) * 100 + "%"; 
+
+    }
+
+
+}
+
